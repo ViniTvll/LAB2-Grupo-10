e3eed42cb8a62856aec4ba2e02dc4d351a53a1cb
Adrian Lopez <adrian.lopez@pucp.edu.pe>
2023-04-04 20:48:25 -0500

    base_proyecto

---
+++ src/main/java/com/example/lab2_grupo10/Lab2Grupo10Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_grupo10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Grupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2Grupo10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab2_grupo10/Lab2Grupo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_grupo10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2Grupo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
